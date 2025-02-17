d794da8c49d9f0badf3eda113f0664140ad65b6a
Jerremy Asuncion <asuncionjerremyph@gmail.com>
2025-02-17 22:23:10 +0800

    Added Navigation Bar

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="flex flex-column sm:flex-row border border-gray-400">
+            {/* Hero Left Side */}
+        </div>
+    );
+};
+
+export default Hero;
