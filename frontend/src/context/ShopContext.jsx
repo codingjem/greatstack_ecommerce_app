aa72cccf4ed48241652b9552eb0f0914d4115883
Jerremy Asuncion <asuncionjerremyph@gmail.com>
2025-02-27 23:57:08 +0800

    Added Cart Page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -44,6 +44,27 @@ const ShopContextProvider = (props) => {
         return totalCount;
     };
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    };
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {}
+            }
+        }
+        return totalAmount;
+    };
+
     const value = {
         products,
         currency,
@@ -55,6 +76,8 @@ const ShopContextProvider = (props) => {
         cartItems,
         addToCart,
         getCartCount,
+        updateQuantity,
+        getCartAmount,
     };
 
     return (
